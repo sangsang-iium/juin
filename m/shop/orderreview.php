560cc262bf943e39a10387791b88d10af95f0567
onesick <onesick@iium.kr>
2024-02-26 15:04:12 +0900

    [상품상세/리뷰] 팝업 내 버튼 정리

--- m/shop/orderreview.php
+++ m/shop/orderreview.php
@@ -4,8 +4,8 @@ include_once("./_common.php");
 if(!$is_member)
     alert_close("로그인 후 작성 가능합니다.");
 
-$tb['title'] = "구매후기 작성";
-include_once(BV_MPATH."/head.sub.php");
+// $tb['title'] = "구매후기 작성";
+// include_once(BV_MPATH."/head.sub.php");
 
 $gs = sql_fetch("select * from shop_goods where index_no='$gs_id'");
 
@@ -21,5 +21,5 @@ set_session("ss_token", $token);
 $form_action_url = BV_HTTPS_MSHOP_URL.'/orderreview_update.php';
 include_once(BV_MTHEME_PATH."/orderreview.skin.php");
 
-include_once(BV_MPATH."/tail.sub.php");
+// include_once(BV_MPATH."/tail.sub.php");
 ?>
