5779b90184bf759d46f05a003d4dcbdb4ac959ff
soyeon <berry@iium.kr>
2024-03-12 15:28:01 +0900

    [앱검수] 장바구니 회원 구분
    20240312

--- m/shop/cart.php
+++ m/shop/cart.php
@@ -5,10 +5,18 @@ include_once(BV_SHOP_PATH.'/settle_naverpay.inc.php');
 $tb['title'] = '장바구니';
 include_once("./_head.php");
 
+// 장바구니 구분 추가 _20240312_SY
+if($is_member) {
+  $sql_add = " AND mb_id = '{$member['id']}' ";
+} else {
+  $sql_add = "";
+}
+
 $sql = " select * 
 		   from shop_cart 
 		  where ct_direct = '$set_cart_id' 
 		    and ct_select = '0' 
+        {$sql_add}
 		  group by gs_id 
 		  order by index_no ";
 $result = sql_query($sql);
