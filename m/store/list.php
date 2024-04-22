6aac41d3fd9b4f070d3af0e480a4e1ef6ed2ad48
onesick <onesick@iium.kr>
2024-04-22 16:01:19 +0900

    [추가플랫폼] 메뉴생성
    -중고장터, 회원사현황, 제휴서비스 메뉴 생성

---
+++ m/store/list.php
@@ -0,0 +1,10 @@
+<?php
+include_once("./_common.php");
+include_once(BV_MPATH."/_head.php"); // 상단
+?>
+
+회원사현황
+
+<?php
+include_once(BV_MPATH."/_tail.php"); // 하단
+?>
---
+++ m/used/list.php
@@ -0,0 +1,10 @@
+<?php
+include_once("./_common.php");
+include_once(BV_MPATH."/_head.php"); // 상단
+?>
+
+중고장터
+
+<?php
+include_once(BV_MPATH."/_tail.php"); // 하단
+?>
