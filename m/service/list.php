3e387b92f42db353b3b9aff3316058323782a8cf
onesick <onesick@iium.kr>
2024-04-26 14:05:43 +0900

    [제휴서비스] 목록 퍼블 완료

--- m/service/list.php
+++ m/service/list.php
@@ -3,7 +3,44 @@ include_once("./_common.php");
 include_once(BV_MPATH."/_head.php"); // 상단
 ?>
 
-제휴서비스
+<div id="contents" class="sub-contents serviceList">
+  <div class="container service-item_list">
+    <!-- 자료 없을때
+    <p class="empty_list">자료가 없습니다.</p>
+    -->
+    <!-- loop { -->
+    <a href="" class="service-item">
+      <div class="cp-banner__round thumb">
+        <img src="/src/img/service/t-service_thumb1.png" alt="주인장 제휴카드" class="fitCover">
+      </div>
+      <div class="service-info">
+        <p class="tRow2 title">
+          <span class="cname">[제휴업체명]</span>
+          <span class="subj">주인장 제휴카드 신청접수</span>
+        </p>
+        <p class="ex">
+          <span class="period">2024.01.01~2024.12.31</span>
+        </p>
+      </div>
+    </a>
+    <!-- } loop -->
+
+    <a href="" class="service-item">
+      <div class="cp-banner__round thumb">
+        <img src="/src/img/service/t-service_thumb2.png" alt="주인장 제휴보험" class="fitCover">
+      </div>
+      <div class="service-info">
+        <p class="tRow2 title">
+          <span class="cname">[제휴업체명]</span>
+          <span class="subj">주인장 제휴보험 신청접수</span>
+        </p>
+        <p class="ex">
+          <span class="period">2024.01.01~2024.12.31</span>
+        </p>
+      </div>
+    </a>
+  </div>
+</div>
 
 <?php
 include_once(BV_MPATH."/_tail.php"); // 하단
