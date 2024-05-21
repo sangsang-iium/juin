626cdcfcdd36ad9fa93cd3c1eeca554a956f627e
soyeon <berry@iium.kr>
2024-05-21 18:00:05 +0900

    [지회/지부] UPDATE 쿼리 수정
    20240521

--- admin/config/branchupdate.php
+++ admin/config/branchupdate.php
@@ -1,14 +1,15 @@
 <?php // 지회 Update Form _20240516_SY
 include_once("./_common.php");
 
-check_demo();
+// check_demo();
 
-check_admin_token();
+// check_admin_token();
 
 $db_table   = "kfia_region";
 $kf_code    = preg_replace("/\s+/", "", $_POST['branch_id']);
 $kf_region1 = $_POST['kf_region1'];
 $kf_region2 = preg_replace("/\s+/", "", $_POST['kf_region2']);
+$now_date = date('Y-m-d H:i:s');
 
 $duChk_sql = " SELECT kf_idx, COUNT(*) AS cnt FROM kfia_region 
                 WHERE kf_region1 = '{$kf_region1}' 
@@ -25,8 +26,8 @@ if($w == '') {
     $db_insert['kf_code']    = $kf_code;
     $db_insert['kf_region1'] = $kf_region1;
     $db_insert['kf_region2'] = $kf_region2;
-    $db_insert['kf_wdate']   = date('Y-m-d H:i:s');
-    $db_insert['kf_udate']   = date('Y-m-d H:i:s');
+    $db_insert['kf_wdate']   = $now_date;
+    $db_insert['kf_udate']   = $now_date;
     
     $INSERT = new IUD_Model;
     $INSERT->insert($db_table, $db_insert);
@@ -41,13 +42,18 @@ if($w == '') {
     alert("이미 등록되어 있는 정보입니다.", $_SERVER['HTTP_REFERER']);
     die();
   } else {
-    $db_update['kf_region1'] = $kf_region1;
-    $db_update['kf_region2'] = $kf_region2;
-    $db_update['kf_udate']   = date('Y-m-d H:i:s');
+    // 수정 _20240521_SY
     $db_where = " WHERE kf_idx = '{$idx}' ";
-
-    $UPDATE = new IUD_Model;
-    $UPDATE->update($db_table, $db_update, $db_where);
+    $data_sel = " SELECT * FROM {$db_table} {$db_where}";
+    $data_row = sql_fetch($data_sel);
+    $upd_sql = " UPDATE {$db_table}
+                    SET kf_region1 = '{$kf_region1}', 
+                        kf_region2 = '{$kf_region2}',
+                        kf_udate   = '{$now_date}'
+                  WHERE kf_region1 = '{$data_row['kf_region1']}' 
+                    AND kf_region2 = '{$data_row['kf_region2']}' 
+               ";
+    $upd_res = sql_query($upd_sql);
 
     alert("지회 정보가 수정되었습니다.", BV_ADMIN_URL."/config.php?$q1&w=u&idx=$idx");
   }
