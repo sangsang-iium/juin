7c5accc86a7a7ffa568c471717e671c684f94705
soyeon <berry@iium.kr>
2024-07-04 18:11:36 +0900

    [채팅 푸시]
    중고장터 채팅 푸시

---
+++ admin/cron.push.php
@@ -0,0 +1,77 @@
+<?php
+include_once "/home/juin/www/common.php";
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+// 쿠폰 만료 푸시 _20240704_SY
+function couponPush() {
+  //  오늘 날짜
+  $timestamp = strtotime("Now");
+  $now = date("Y-m-d", $timestamp);
+
+  // 미사용 쿠폰
+  $unUsedCoupon_sel = " SELECT cp.*, mm.fcm_token, 
+                          CASE 
+                              WHEN cp.cp_inv_type = '1' THEN DATE_ADD(cp.cp_wdate, INTERVAL cp.cp_inv_day DAY)
+                              ELSE cp.cp_inv_edate
+                          END AS expiration_date
+                          FROM shop_coupon_log AS cp
+                    LEFT JOIN shop_member AS mm ON (cp.mb_id = mm.id)
+                        WHERE cp.mb_use='0' 
+                          AND ( 	
+                                (cp.cp_inv_type='0' AND (cp.cp_inv_edate = '9999999999' OR cp.cp_inv_edate > CURDATE() ) ) OR 
+                                (cp.cp_inv_type='1' AND DATE_ADD(cp.cp_wdate, INTERVAL cp.cp_inv_day DAY) > NOW()) 
+                              ) ";
+  $unUsedCoupon_res = sql_query($unUsedCoupon_sel);
+
+  while($unUsedCoupon_row = sql_fetch_array($unUsedCoupon_res)) {
+    $cp_inv_type  = $unUsedCoupon_row['cp_inv_type'];
+    $expiry_date  = $unUsedCoupon_row['expiration_date'];
+    $cp_inv_edate = $unUsedCoupon_row['cp_inv_edate'];
+    // $fcm_token    = $unUsedCoupon_row['fcm_token'];
+
+    // 테스트용 토큰
+    $fcm_token = "eQWmi-2FT8eL5C_SPtGi0E:APA91bGUA5K1gYAcmTY1Pz-Yn6BJn65OpmAsCT_RUDLpZ3wNPPzg10qbanZXcAdPOQ42uX7kpVs0Yvx_oQ5wNYay9nxlZSeNgL2ViRpVFefMHjGxCUnPW47rAwvYrxOKG7l9x-ZfESBA";
+
+    if($cp_inv_type == '1' ||  $cp_inv_edate != '9999999999'){
+    
+      // 만료일 1달 전, 1주일 전 날짜 계산
+      $beforeMonths = date("Y-m-d", strtotime($expiry_date . " -1 months"));
+      $beforeWeeks = date("Y-m-d", strtotime($expiry_date . " -7 days"));
+      $limit_date  = substr($expiry_date, 0, 10);
+
+      // 만료 1달 전
+      if ($now >= $beforeMonths && $now < date("Y-m-d", strtotime($beforeMonths . " +1 day"))) {
+        $message = [
+          'token' => $fcm_token,
+          'title' => '쿠폰 만료 알림',
+          'body' => "보유하신 \"{$unUsedCoupon_row['cp_subject']}\" 쿠폰 만료일이 1달 남았습니다."
+        ];
+        $response = sendFCMMessage($message);
+      }
+
+      // 만료 일주일 전
+      if ($now >= $beforeWeeks && $now < date("Y-m-d", strtotime($beforeWeeks . " +1 day"))) {
+        $message = [
+          'token' => $fcm_token,
+          'title' => '쿠폰 만료 알림',
+          'body' => "보유하신 \"{$unUsedCoupon_row['cp_subject']}\" 쿠폰 만료일이 일주일 남았습니다."
+        ];
+        $response = sendFCMMessage($message);
+      }
+
+      // 만료 하루 전
+      if ($now == date("Y-m-d", strtotime($limit_date. " -1 day"))) {
+        $message = [
+          'token' => $fcm_token,
+          'title' => '쿠폰 만료 알림',
+          'body' => "보유하신 \"{$unUsedCoupon_row['cp_subject']}\" 쿠폰 만료일이 하루 남았습니다."
+        ];
+        $response = sendFCMMessage($message);
+      }
+    } 
+    
+  }
+  return $response;
+}
--- m/used/ajax.chat_insert.php
+++ m/used/ajax.chat_insert.php
@@ -7,5 +7,55 @@ $content = trim($_POST['content']);
 
 sql_query("insert into shop_used_chatd set pno={$chatno}, mb_id='{$member['id']}', content='$content', regdate='".BV_TIME_YMDHIS."'");
 sql_query("update shop_used_chat set lasttime='".BV_SERVER_TIME."' where no={$chatno}");
+
+
+
+// 채팅 PUSH _20240704_SY
+$chat_sel = " SELECT room.mb_id AS seller, sub.mb_id AS buyer, chat.*
+                FROM shop_used AS room, shop_used_chat AS sub, shop_used_chatd AS chat
+               WHERE room.no = sub.pno
+                 AND sub.no  = chat.pno
+                 AND room.no = '{$chatno}'
+            ORDER BY chat.`no` DESC 
+                LIMIT 1 ";
+$chat_row = sql_fetch($chat_sel);
+
+$seller = $chat_row['seller'];
+$buyer  = $chat_row['buyer'];
+$sender = $chat_row['mb_id'];
+$mread  = $chat_row['mread'];
+$uread  = $chat_row['uread'];
+
+if($seller == $sender && $uread != '1') {
+  // 판매자가 채팅 보냈을 때
+  $token_sel = " SELECT fcm_token FROM shop_member WHERE id = '{$buyer}' ";
+  $token_row = sql_fetch($token_sel);
+  // $fcm_token = $token_row['fcm_token'];
+  // 테스트 토큰
+  $fcm_token = "eQWmi-2FT8eL5C_SPtGi0E:APA91bGUA5K1gYAcmTY1Pz-Yn6BJn65OpmAsCT_RUDLpZ3wNPPzg10qbanZXcAdPOQ42uX7kpVs0Yvx_oQ5wNYay9nxlZSeNgL2ViRpVFefMHjGxCUnPW47rAwvYrxOKG7l9x-ZfESBA";
+
+  $message = [
+    'token' => $fcm_token,
+    'title' => '채팅알림',
+    'body'  => '주인장 중고장터 메신저가 도착했습니다.'
+  ];
+  $response = sendFCMMessage($message);
+
+} else if($buyer == $sender && $mread != '1') {
+  // 구매자가 채팅 보냈을 때
+  $token_sel = " SELECT fcm_token FROM shop_member WHERE id = '{$seller}' ";
+  $token_row = sql_fetch($token_sel);
+  // $token = $token_row['fcm_token'];
+  // 테스트 토큰
+  $fcm_token = "eQWmi-2FT8eL5C_SPtGi0E:APA91bGUA5K1gYAcmTY1Pz-Yn6BJn65OpmAsCT_RUDLpZ3wNPPzg10qbanZXcAdPOQ42uX7kpVs0Yvx_oQ5wNYay9nxlZSeNgL2ViRpVFefMHjGxCUnPW47rAwvYrxOKG7l9x-ZfESBA";
+
+  $message = [
+      'token' => $fcm_token,
+      'title' => '채팅알림',
+      'body'  => '주인장 중고장터 메신저가 도착했습니다.'
+  ];
+  $response = sendFCMMessage($message);
+}
+
 exit;
 ?>
