be05be0f60c82967597fb668d6a3d83e54d15eb2
jinho <jjh@iium.kr>
2024-07-11 16:27:13 +0900

    일부 수정

--- m/theme/basic/find_id.skin.php
+++ m/theme/basic/find_id.skin.php
@@ -98,9 +98,6 @@ if(!defined("_BLUEVATION_")) exit; // 개별 페이지 접근 불가
 
   // 부모 창에서 메시지 수신
   // window.addEventListener("message", function(event) {
-  //   const certData = event.data;
-  //   console.log(event.data);
-  //   console.log("Received data from child:", certData.message);
     
   // //   // const allowedOrigin = "<?php //echo $_SERVER['HTTP_HOST']; ?>";
   // //   // const eventOrigin = event.origin.replace(/^https?:\/\//, ''); // 프로토콜 제거
@@ -121,6 +118,7 @@ if(!defined("_BLUEVATION_")) exit; // 개별 페이지 접근 불가
   //   }
   // });
 
+
 // 앱 에서 처리 할 필요 없게 수정 _20240708_SY
 $(function() {
     setInterval(function() {
@@ -134,4 +132,5 @@ $(function() {
         }
     }, 1500); // 2초마다 실행
 });
+
 </script> 
